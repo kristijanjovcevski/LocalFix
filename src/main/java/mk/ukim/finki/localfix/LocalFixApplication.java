cf10d0fa49fc9a7d98b98180813e675f0cdfcb28
Filip Kraljevski <filipce001@gmail.com>
2024-03-29 16:10:02 +0100

    initial

---
+++ src/main/java/mk/ukim/finki/localfix/LocalFixApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.localfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalFixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/localfix/LocalFixApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.localfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
