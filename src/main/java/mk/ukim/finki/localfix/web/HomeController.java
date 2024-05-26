c50bcee5e64f90839893dc51fb84afff51da951d
Georgina Mihailova <georginamihailova@hotmail.com>
2024-05-26 20:37:06 +0200

    modified the about-us section to display the username

--- src/main/java/mk/ukim/finki/localfix/web/HomeController.java
+++ src/main/java/mk/ukim/finki/localfix/web/HomeController.java
@@ -1,15 +1,26 @@
 package mk.ukim.finki.localfix.web;
 
+import mk.ukim.finki.localfix.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.*;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    public String Home(){
+    public String Home(Model model, HttpServletRequest request){
+        String username = request.getRemoteUser();
+        model.addAttribute("person",username);
         return "about-us";
     }
 }
